5ec40483ae67c0734b09a7221ee928bca51b9bcd
mfmontess <mmontess@ucentral.edu.co>
2017-06-20 22:51:47 -0500

    ajustes

--- Analizador/src/Controller/Principal.java
+++ Analizador/src/Controller/Principal.java
@@ -1,7 +1,6 @@
 package Controller;
 
 import View.DashBoard;
-import View.Vista;
 
 /**
  * Compiladores
@@ -11,15 +10,9 @@ import View.Vista;
 public class Principal {
 
 	public static void main(String[] args) {
-		/*Vista miVentana = new Vista();
-		miVentana.setLocationRelativeTo(null);
-		miVentana.setResizable(false);
-		miVentana.setVisible(true);*/
-		
 		DashBoard tablero = new DashBoard();
 		tablero.setLocationRelativeTo(null);
 		tablero.setResizable(false);
 		tablero.setVisible(true);
-		
 	}
 }
